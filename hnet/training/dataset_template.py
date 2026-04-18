81db2bcbc93fd19325d5b0066cb478e354bdcd68
windows_on_wsl <otomijuf.004@gmail.com>
2026-04-19 00:25:30 +0900

    add 30 dataset template

--- hnet/training/dataset_template.py
+++ hnet/training/dataset_template.py
@@ -7,6 +7,7 @@ SOURCES_JA8_EN1_CODE1: list[DatasetSource] = [
         config_name="small_tokens_cleaned",
         split="train",
         take_examples=68_000,
+        skip_examples=10_000,  ## 先頭10000件スキップが推奨
     ),
     DatasetSource(
         name="wikimedia/wikipedia",
@@ -41,6 +42,7 @@ SOURCES_JA9_EN0_CODE1: list[DatasetSource] = [
         config_name="small_tokens_cleaned",
         split="train",
         take_examples=76_500,
+        skip_examples=10_000,  ## 先頭10000件スキップが推奨
     ),
     DatasetSource(
         name="wikimedia/wikipedia",
@@ -62,6 +64,7 @@ SOURCES_JA45_EN45_CODE1: list[DatasetSource] = [
         config_name="small_tokens_cleaned",
         split="train",
         take_examples=36_000,
+        skip_examples=10_000,  ## 先頭10000件スキップが推奨
     ),
     DatasetSource(
         name="wikimedia/wikipedia",
@@ -98,6 +101,7 @@ SOURCES_JA8_EN1_CODE1_10: list[DatasetSource] = [
         config_name="small_tokens_cleaned",
         split="train",
         take_examples=680_000,
+        skip_examples=10_000,  ## 先頭10000件スキップが推奨
     ),
     DatasetSource(
         name="wikimedia/wikipedia",
@@ -132,6 +136,7 @@ SOURCES_JA9_EN0_CODE1_10: list[DatasetSource] = [
         config_name="small_tokens_cleaned",
         split="train",
         take_examples=765_000,
+        skip_examples=10_000,  ## 先頭10000件スキップが推奨
     ),
     DatasetSource(
         name="wikimedia/wikipedia",
@@ -153,6 +158,7 @@ SOURCES_JA45_EN45_CODE1_10: list[DatasetSource] = [
         config_name="small_tokens_cleaned",
         split="train",
         take_examples=360_000,
+        skip_examples=10_000,  ## 先頭10000件スキップが推奨
     ),
     DatasetSource(
         name="wikimedia/wikipedia",
@@ -191,6 +197,7 @@ SOURCES_JA8_EN1_CODE1_SMALL: list[DatasetSource] = [
         config_name="small_tokens_cleaned",
         split="train",
         take_examples=68_000,
+        skip_examples=10_000,  ## 先頭10000件スキップが推奨
     ),
     DatasetSource(
         name="wikimedia/wikipedia",
@@ -223,14 +230,14 @@ SOURCES_JA8_EN1_CODE1_SMALL: list[DatasetSource] = [
 ]
 
 ## 8:1:1
-## byte換算で8B
+## byte換算で12B
 SOURCES_JA8_EN1_CODE1_20: list[DatasetSource] = [
     DatasetSource(
         name="hotchpotch/fineweb-2-edu-japanese",
         config_name="small_tokens_cleaned",
         split="train",
-        take_examples=1260_000,
-        skip_examples=68_000,
+        take_examples=1_260_000,
+        skip_examples=78_000,
     ),
     DatasetSource(
         name="wikimedia/wikipedia",
@@ -244,7 +251,7 @@ SOURCES_JA8_EN1_CODE1_20: list[DatasetSource] = [
         config_name="sample-10BT",
         split="train",
         take_examples=160_000,
-        skip_examples=8_000,
+        skip_examples=10_000,  ## 先頭10000件スキップが推奨
     ),
     DatasetSource(
         name="wikimedia/wikipedia",
@@ -257,6 +264,52 @@ SOURCES_JA8_EN1_CODE1_20: list[DatasetSource] = [
         name="codeparrot/codeparrot-clean",
         split="train",
         take_examples=200_000,
+        skip_examples=10_000,
+    ),
+    DatasetSource(
+        name="if001/aozorabunko-clean-sin",
+        split="train",
+        take_examples=5_000,
+        skip_examples=5_000,
+    ),
+]
+
+
+## 8:1:1
+## byte換算で12B
+SOURCES_JA8_EN1_CODE1_40: list[DatasetSource] = [
+    DatasetSource(
+        name="hotchpotch/fineweb-2-edu-japanese",
+        config_name="small_tokens_cleaned",
+        split="train",
+        take_examples=680_000 * 3,
+        skip_examples=78_000,
+    ),
+    DatasetSource(
+        name="wikimedia/wikipedia",
+        config_name="20231101.ja",
+        split="train",
+        take_examples=120_000 * 3,
+        skip_examples=12_000,
+    ),
+    DatasetSource(
+        name="HuggingFaceFW/fineweb-edu",
+        config_name="sample-10BT",
+        split="train",
+        take_examples=80_000 * 3,
+        skip_examples=10_000,  ## 先頭10000件スキップが推奨
+    ),
+    DatasetSource(
+        name="wikimedia/wikipedia",
+        config_name="20231101.en",
+        split="train",
+        take_examples=20_000 * 3,
+        skip_examples=2_000,
+    ),
+    DatasetSource(
+        name="codeparrot/codeparrot-clean",
+        split="train",
+        take_examples=100_000 * 3,
         skip_examples=10_000,
     ),
     DatasetSource(

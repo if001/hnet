f48119492b4c5ca53982774b227096cadbc24645
windows_on_wsl <otomijuf.004@gmail.com>
2026-07-03 19:16:47 +0900

    add dataset template

--- hnet/training/dataset_template.py
+++ hnet/training/dataset_template.py
@@ -92,6 +92,39 @@ SOURCES_JA45_EN45_CODE1: list[DatasetSource] = [
 ]
 
 
+SOURCES_JA8_EN1_CODE1: list[DatasetSource] = [
+    DatasetSource(
+        name="hotchpotch/fineweb-2-edu-japanese",
+        config_name="small_tokens_cleaned",
+        split="train",
+        take_examples=68_000 * 5,
+        skip_examples=10_000,  ## 先頭10000件スキップが推奨
+    ),
+    DatasetSource(
+        name="wikimedia/wikipedia",
+        config_name="20231101.ja",
+        split="train",
+        take_examples=12_000 * 5,
+    ),
+    DatasetSource(
+        name="HuggingFaceFW/fineweb-edu",
+        config_name="sample-10BT",
+        split="train",
+        take_examples=8_000 * 5,
+    ),
+    DatasetSource(
+        name="wikimedia/wikipedia",
+        config_name="20231101.en",
+        split="train",
+        take_examples=2_000 * 5,
+    ),
+    DatasetSource(
+        name="codeparrot/codeparrot-clean",
+        split="train",
+        take_examples=10_000 * 5,
+    ),
+]
+
 ## 8:1:1
 ## chatGPTの概算では通常のtokenizerで1B Token程度
 ## byte換算で4B
